cd2a76674d8a65a64702492bf6ac59d82508d0b9
jlarata <ariel@cfee.gob.ar>
2022-06-12 21:21:23 -0300

    ...

--- scripts.js
+++ scripts.js
@@ -1,17 +1,26 @@
 
 
-/*function randomStudents() {
-  alert("pará loca, recién armé el esqueleto!")
- numeros = document.querySelectorAll('.columna2');
-}*/
-
 var randNums = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22];
+var primera = 0;
 
 function randomStudents () {
-  var numeros = document.querySelectorAll('.columna2');
 
-  for(var i=0; i<numeros.length; i++){
-      /*data = Math.floor(Math.random()*(randNums.length));*/
+  numeros = document.querySelectorAll('.columna2');
+
+  if (primera == 1) {
+    for(var i=0; i<numeros.length; i++){
+    numeros[i].innerHTML='';
+    }
+    meterNumeros();
+    } else {
+    meterNumeros();
+    }
+  }
+
+
+function meterNumeros() {
+
+      for(var i=0; i<numeros.length; i++){
       var data = randNums[Math.floor(Math.random() * randNums.length)];
       var node = document.createTextNode(data);
       numeros[i].appendChild(node);
@@ -19,4 +28,7 @@ function randomStudents () {
       randNums.splice(index, 1)
       }
 
-}
+      randNums = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22]
+      primera = 1;
+
+  }
