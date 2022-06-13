92810060a4a7d23959f9b7495f5fe61b972332c0
Ariel <ariel@cfee.gob.ar>
2022-06-13 13:15:45 -0300

    ...

--- scripts.js
+++ scripts.js
@@ -1,7 +1,46 @@
 
 
 var randNums = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22];
+var alumnes = []
 var primera = 0;
+
+
+function masAzar() {
+  alumnxs = document.querySelectorAll('.columna1');
+
+  /* arma un array tomando los valores de la columna1*/
+  for(var i=0; i<alumnxs.length; i++){
+    alumne = alumnxs[i].innerHTML;
+    reordenarAlumnes(alumne);
+    }
+
+  /* borra los valores de la columna 1 */
+  for(var i=0; i<alumnxs.length; i++){
+    alumnxs[i].innerHTML = ''
+    }
+
+  /* vuelve a agregar los valores */
+
+  for(var i=0; i<alumnxs.length; i++){
+
+    var data = alumnes[Math.floor(Math.random() * alumnes.length)];
+    alumnes.splice(data, 1);
+    var node = document.createTextNode(data);
+    alumnxs[i].appendChild(node);
+
+        }
+
+
+  }
+
+
+
+
+function reordenarAlumnes(alumnx) {
+  alumnes.splice(0, 0, alumnx)
+  }
+
+
 
 function randomStudents () {
 
